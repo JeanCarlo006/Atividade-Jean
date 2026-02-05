b6c65a661f93a68a7c54250e8151662adcee8710
JeanCarlo006 <jeancarlotorresdeoliveira3@gmail.com>
2026-02-05 20:33:15 -0300

    novo

---
+++ idex.php
@@ -0,0 +1,35 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha primeira página</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- CABEÇALHO DA MINHA PÁGINA  -->
+        <header>
+            <!-- TÍTULO -->
+            <h1>Esse é meu primeiro projeto WEB</h1>
+            <p>Me chamo Jean aluno do 3° período</p>
+        </header>
+
+        <!-- MENU DE NAVEGAÇÃO -->
+        <nav>
+            <ul>
+                <li><a href="#">Tags HTML</a></li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+
+        </nav>
+
+
+
+    </div>
+
+</body>
+
+</html>
